76fc014fb7c7e51a64e1d2a12b695e87838348ae
Tripp Wickersham <rwickersham@rmn.com>
2015-03-15 13:13:19 -0500

    data is now being persisted on the local filesystem

--- server/app.py
+++ server/app.py
@@ -1,30 +1,62 @@
 from flask import Flask, request, abort
+import os
+
 app = Flask(__name__)
- 
-responseMap = {
-      'hello': 'Kasey does what he wants'
-    }
-  
-@app.route('/api/<path:endpoint>', methods=['POST', 'PUT'])
-def add_endpoint(endpoint):
-  responseMap[endpoint] = request.data
-  return "Endpoint {} added!\nResponse: {}\n".format(endpoint, responseMap[endpoint])
-       
+base_directory = "data"
+base_filepath = "./" + base_directory + "/"
+
+@app.route('/api/<path:endpoint>', methods=['POST', 'PUT'])             
+def upsert_endpoint(endpoint):
+  print("yup here")
+  data_file = base_filepath + endpoint + "/data.txt"
+  print(data_file)
+  if os.path.exists(base_filepath + endpoint) and os.path.exists(data_file):
+    f = open(data_file, "w")
+    f.seek(0)
+    f.write(request.data)
+    f.truncate()
+    f.close()
+    return "Endpoint {} updated!\nResponse: {}\n".format(endpoint, request.data)
+  elif os.path.exists(base_filepath + endpoint) and not os.path.exists(data_file):
+    # children endpoints already exist, creating new file
+    f = open(data_file, "w")
+    f.write(request.data)
+    f.close()
+    return "Endpoint {} added!\nResponse: {}\n".format(endpoint, request.data)
+  else:
+    os.makedirs(base_filepath + endpoint)
+    f = open(data_file, "w")
+    f.write(request.data)
+    f.close()
+    return "Endpoint {} added!\nResponse: {}\n".format(endpoint, request.data)
+
 @app.route('/api/<path:endpoint>', methods=['DELETE'])
-def del_endpoint(endpoint):
-  del responseMap[endpoint]
-  return "Endpoint {} deleted!\n".format(endpoint)
-           
-@app.route('/api/<path:endpoint>', methods=['GET'])
-def get_endpoint(endpoint):
-  if endpoint not in responseMap:
+def delete_endpoint(endpoint):
+  data_file = base_filepath + endpoint + "/data.txt"
+  if os.path.exists(data_file):
+    os.remove(data_file)
+    for dirpath, dirnames, files in os.walk(base_filepath + endpoint):
+      if not dirnames:
+        os.rmdir(base_filepath + endpoint)
+      return "Endpoint {} deleted!\n".format(endpoint)
+  else:
     abort(404)
 
-  return responseMap[endpoint]
-             
+@app.route('/api/<path:endpoint>', methods=['GET'])
+def get_endpoint(endpoint):
+  data_file = base_filepath + endpoint + "/data.txt"
+  if os.path.exists(data_file):
+    f = open(data_file, "r")
+    data = f.read()
+    f.close()
+    return data
+  else:
+    abort(404)
 
 def main():
-  app.run(host="0.0.0.0", port=5000)
+  app.run(host="0.0.0.0", port=5000, debug=True)
 
 if __name__ == '__main__':
   main()
+
+
